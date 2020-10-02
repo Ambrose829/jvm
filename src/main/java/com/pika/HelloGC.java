fb6855e6ef168965722931cc0c46e5db67ca809d
Ambrose829 <l>
2020-10-02 21:56:28 +0800

    基本参数入门case

---
+++ src/main/java/com/pika/HelloGC.java
@@ -0,0 +1,13 @@
+package com.pika;
+
+/**
+ * @author Pika
+ * @create 2020/10/2
+ * @since 1.0.0
+ */
+public class HelloGC {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("helloGC******");
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+}
---
+++ src/main/java/com/pika/T.java
@@ -0,0 +1,15 @@
+package com.pika;
+
+/**
+ * @author Pika
+ * @create 2020/10/2
+ * @since 1.0.0
+ */
+public class T {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+        int retValue= a + b;
+        System.out.println("retValue*** : " + retValue);
+    }
+}
